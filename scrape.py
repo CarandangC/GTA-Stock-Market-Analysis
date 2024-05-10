c5330a731f4254b8114c1def9bda1499f9ef09b3
Carlos <carlos.carandang@torontomu.ca>
2024-05-09 17:03:44 -0700

    Added full page webcraping along with storing data

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 
 prices = []
@@ -24,7 +25,7 @@ for view in listing:
     bed_and_bath = view.find("div", class_="property-details_detailsWrapper__6W1XU listing-card_propertyDetailsRoot__SC_jl").text
     temp = bed_and_bath.split("bed")
     bed = temp[0] + "bed"
-    bath = temp[1].lstrip()
+    bath = temp[1].strip()
     address = view.find("div", class_="listing-address_root__g9lT5 listing-card_address__6GsHt").text
     link = view.find("a", class_ ="listing-card_listingCard__lc4CL")["href"]
 
@@ -34,10 +35,19 @@ for view in listing:
     else:
         address_type = False
 
-    print(price + "\n")
-    print(bed + "\n")
-    print(bath + "\n")
-    print(address + "\n")
-    print(link + "\n")
-    print(address_type)
+    prices.append(price)
+    beds.append(bed)
+    baths.append(bath) 
+    addresses.append(address) 
+    links.append(link)
+    estate_type.append(address_type)
     
+#Exporting data into a csv using pandas
+
+data = {"Price" : prices, "Address" : addresses, "Beds" : beds, "Baths" : baths, "Type" : estate_type, "Link" : link}
+df = pd.DataFrame(data)
+df["Type"] = df["Type"].replace({True: "Condo", False: "House"})
+
+
+#convert data into csv file
+df.to_csv("Toronto_listings.csv", sep=',', index=False, encoding='utf-8')
